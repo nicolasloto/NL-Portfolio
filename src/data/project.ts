b2b053cf3438a79a9b6a41d43af45d6642b03043
Nicolás Loto <busslxtx@gmail.com>
2025-03-04 23:44:14 -0300

    formato, mejorar de imagenes, redacción

--- src/data/project.ts
+++ src/data/project.ts
@@ -6,21 +6,25 @@ export const projects: Project[] = [
   {
     id: 1,
     title: "E-Commerce Store",
-    description: "Created a responsive e-commerce store with product listings, detailed pages, and a seamless shopping cart. Integrated secure authentication and efficient data management for a modern online shopping experience.",
+    description:
+      "A responsive e-commerce platform designed to provide a seamless and modern shopping experience. It features product listings, detailed pages, and an intuitive shopping cart. Secure authentication and efficient data management were implemented to ensure optimal performance and security.",
     image: qualityLanding,
     tags: ["React", "TailwindCSS", "Node.js"],
     demoUrl: "https://qualitywines-store.vercel.app/",
-    githubUrl: "https://github.com/Grupo-ITSE-2023-para-Quality/Store-quality-wines",
+    githubUrl:
+      "https://github.com/Grupo-ITSE-2023-para-Quality/Store-quality-wines",
     featured: true,
   },
   {
     id: 2,
     title: "Admin Dashboard",
-    description: "Developed a robust dashboard to manage products, orders, and user data. Enabled real-time updates, role-based access, and streamlined operations for efficient management.",
+    description:
+      "A comprehensive platform for managing products, orders, and user data. It includes real-time updates, role-based access, and optimized workflows to streamline operations and enhance efficiency.",
     image: qualityDashboard,
     tags: ["CRUD", "Clerk"],
     demoUrl: "https://qualitywines-admin.vercel.app/",
-    githubUrl: "https://github.com/Grupo-ITSE-2023-para-Quality/Dashboard-Quality-Wines",
+    githubUrl:
+      "https://github.com/Grupo-ITSE-2023-para-Quality/Dashboard-Quality-Wines",
     featured: false,
   },
 ];
